3c89f6bacee7c2a6d3c87ffb5dd9e9f4ca1c70ce
Kyohei Hamada <kyohei.hamada@gmail.com>
2012-10-04 19:09:53 +0900

    added シェルスクリプト用のディレクトリ

---
+++ sh/showLoginTime.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# @feature
+#  月ごとのログイン時間を集計する
+#  年は考慮されていない
+# @author kyohei.hamada
+last | awk --assign name="$1" '
+    $1 == name {
+        date = $5
+        h[date] = h[date] + substr($10, 2, 2) # hour
+        m[date] = m[date] + substr($10, 5, 2) # minite
+    }
+    END {
+        for (i in m) {
+            h[i] = h[i] + int(m[i] / 60)
+            m[i] = m[i] % 60
+        }
+        for (i in h) printf("(%s) %02d:%02d\n", i, h[i], m[i])
+    }
+'
