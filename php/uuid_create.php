09450350479449127d85ae3f51b5babfd28bb0d3
Kyohei Hamada <kyohei.hamada@gmail.com>
2012-12-20 17:24:00 +0900

    commit

---
+++ php/uuid_create.php
@@ -0,0 +1,8 @@
+<?php
+// uuid を生成する string
+$uuid_string = uuid_create(UUID_TYPE_RANDOM);
+var_dump($uuid_string);
+
+// uuid を 16進数 (16 byte) で表示する
+$uuid_bin = pack("h*",  str_replace('-',  '',  $uuid_string));
+var_dump($uuid_bin);
