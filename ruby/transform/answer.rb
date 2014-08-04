9697140750bb66f625ff6e349727c99008246ff1
Kyohei Hamada <kyohei.hamada@gmail.com>
2014-08-05 00:57:13 +0900

    add

---
+++ ruby/transform/answer.rb
@@ -0,0 +1,34 @@
+require 'set'
+require 'debugger'
+
+class Hoge
+
+  def initialize start, goal
+    @start = start
+    @goal = goal
+    @dic = Set.new(open('./dictionary.txt').read.split("\n"))
+    @already_set = Set.new([start])
+  end
+
+  def next_set current=@start
+    lists = (0...current.size).map { |i|
+      ('a'..'z').to_a.delete_if{|c| c == current[i]}.map { |c|
+        chars = current.chars
+        chars[i] = c
+        target = chars.join("")
+        return '見つかった' if target == @goal
+        target if ! @already_set.include?(target) && @dic.include?(target)
+      }.compact
+    }.select{|d| ! d.empty?}.flatten
+
+    lists.each{|list| @already_set.add(list)}
+
+    p lists
+
+    lists.each{|l| next_set(l)}
+  end
+end
+
+start, goal = 'free', 'work'
+hoge = Hoge.new(start, goal)
+p hoge.next_set
