ce25ced9aa9260df1fd7c35f44abf01ed09264c2
Kyohei Hamada <kyohei.hamada@gmail.com>
2019-05-07 09:36:32 +0900

    commit

---
+++ swift/turorial.swift
@@ -0,0 +1,481 @@
+// swift tutorial
+// https://employment.en-japan.com/engineerhub/entry/2017/05/25/110000
+
+// imutable
+let immutableStr = "hello"
+assert(immutableStr == "hello")
+
+// mutable
+var mutableStr = "hello"
+assert(immutableStr == "hello")
+
+// 型指定
+var mutableStr2: String = "hello"
+assert(immutableStr == "hello")
+
+// cast
+let thisYear = "This year is " + String(2017)
+assert(thisYear == "This year is 2017")
+
+// 変数展開
+let year = 2017
+let nextYear = "Next year is \(year + 1)"
+assert(nextYear == "Next year is 2018")
+
+// Array
+let itemsArray = ["foo", "bar", "baz"]
+assert(itemsArray == ["foo", "bar", "baz"])
+
+let itemsArrayString: [String] = ["1", "3", "5"]
+assert(itemsArrayString == ["1", "3", "5"])
+
+let itemsArrayInt: [Int] = [1, 3, 5]
+assert(itemsArrayInt == [1, 3, 5])
+
+// Dictionary
+let itemsDictionary = [
+  "foo": "FOO",
+  "bar": "BAR",
+  "baz": "BAZ"
+]
+assert(itemsDictionary == [
+  "foo": "FOO",
+  "bar": "BAR",
+  "baz": "BAZ"
+])
+
+// 型指定
+let itemsDictionaryString: [String: String] = [
+  "foo": "FOO",
+  "bar": "BAR",
+  "baz": "BAZ"
+]
+assert(itemsDictionary == [
+  "foo": "FOO",
+  "bar": "BAR",
+  "baz": "BAZ"
+])
+
+// nest
+
+let nestedDictionary: [String: [String: Int]] = [
+  "hoge": [
+    "fuga": 1,
+    "piyo": 2,
+  ]
+]
+assert(nestedDictionary == [
+  "hoge": [
+    "fuga": 1,
+    "piyo": 2,
+  ]
+])
+
+// for文, if文
+let list = [3, 7, 9, 12, 8, 5]
+var result: [String] = []
+for number in list {
+  if number % 2 == 0 {
+    result += ["number \(number) is even"]
+  } else {
+    result += ["number \(number) is odd"]
+  }
+}
+assert(result[0] == "number 3 is odd")
+assert(result[1] == "number 7 is odd")
+assert(result[2] == "number 9 is odd")
+assert(result[3] == "number 12 is even")
+assert(result[4] == "number 8 is even")
+assert(result[5] == "number 5 is odd")
+
+// while文
+var number = 1
+var result2: [Int] = []
+while number < 10 {
+  result2 += [number]
+  number += 1
+}
+assert(result2 == [1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+// switch
+let age = 17
+var result3: String
+switch age {
+case 0...6:
+  result3 = "kindergarden kid"
+case 7...12:
+  result3 = "primary school student"
+case 13...15:
+  result3 = "junior high school student"
+case 16...18:
+  result3 = "high school student"
+case 19...22:
+  result3 = "college student"
+default:
+  result3 = "business person"
+}
+assert(result3 == "high school student")
+
+
+// function
+func greet(expression: String, person: String) -> String {
+  return "\(expression) \(person)."
+}
+assert(greet(expression: "Hello", person: "Mike") == "Hello Mike.")
+
+// closure
+func incrementer() -> ( () -> Int ) {
+  var count = 0
+  func increment() -> Int {
+    count += 1
+    return count
+  }
+  return increment
+}
+var counter = incrementer()
+assert(counter() == 1)
+assert(counter() == 2)
+assert(counter() == 3)
+
+// closure 2
+func incrementerWithAnonymousFunc() -> ( () -> Int ) {
+  var count = 0
+  return { () -> Int in // in?!?!?!!
+    count += 1
+    return count
+  }
+}
+var counter2 = incrementerWithAnonymousFunc()
+assert(counter2() == 1)
+assert(counter2() == 2)
+assert(counter2() == 3)
+
+// closure 3
+func numbersMap(list: [Int], condition: (Int) -> Int) -> [Int] {
+  var numbers: [Int] = []
+  for item in list {
+    numbers.append( condition(item) )
+  }
+  return numbers
+}
+
+var items: [Int] = [1, 2, 3, 4, 5]
+assert(
+  numbersMap(list: items, condition: { (number: Int) -> Int in number * 2 })
+  == [2, 4, 6, 8, 10]
+)
+
+// map, filter, reduce
+var numbers = [3, 7, 9, 12, 8, 5]
+assert(
+  numbers.map({ (number: Int) -> Int in return number * 2 })
+  == [6, 14, 18, 24, 16, 10]
+)
+assert(
+  numbers.filter({ (number: Int) -> Bool in return number % 2 == 1 })
+  == [3, 7, 9, 5]
+)
+assert(
+  numbers.reduce(0, { (total: Int, number: Int) -> Int in return total + number })
+  == 44
+)
+
+// map, filter, reduce (型省略)
+assert(numbers.map{ number in number * 2 } == [6, 14, 18, 24, 16, 10])
+assert(numbers.filter{ number in number % 2 == 1 } == [3, 7, 9, 5])
+assert(numbers.reduce(0){ total, number in total + number } == 44)
+
+
+// Class
+class MyApp {
+  class Shape {
+    var name: String
+
+    init(name: String) {
+      self.name = name
+    }
+  }
+
+  // inheritance
+  class Rectangle: Shape {
+    var width: Double
+    var height: Double
+
+    init(name: String, width: Double, height: Double) {
+      self.width = width
+      self.height = height
+      super.init(name: name)
+    }
+
+    func area() -> Double {
+      return width * height
+    }
+  }
+
+  // inheritance
+  class Triangle: Shape {
+    var bottom: Double
+    var height: Double
+
+    init(name: String, bottom: Double, height: Double) {
+      self.bottom = bottom
+      self.height = height
+      super.init(name: name)
+    }
+
+    func area() -> Double {
+      return bottom * height / 2.0
+    }
+  }
+}
+var shape = MyApp.Shape(name: "My Sharp")
+assert(shape.name == "My Sharp")
+
+var square = MyApp.Rectangle(name: "My Square", width: 7.5, height: 7.5)
+assert(square.name == "My Square")
+assert(square.area() == 56.25)
+
+var triangle = MyApp.Triangle(name: "My Triangle", bottom: 10, height: 8)
+assert(triangle.name   == "My Triangle")
+assert(triangle.area() == 40)
+
+// Optional
+var optionalInt: Int? = 5
+// var optionalInt: Optional<Int> = 5
+optionalInt = nil
+assert(optionalInt == nil)
+
+var optionalString: String? = "hoge"
+// var optionalString: Optional<String> = "hoge"
+optionalString = nil
+assert(optionalString == nil)
+
+// Optional Binding (if)
+func hello(str: String?) -> String {
+  let optionalstr: String? = str
+  if let unwrappedstr = optionalstr {
+    return unwrappedstr.uppercased()
+  } else {
+    return ""
+  }
+}
+assert(hello(str: "hello") == "HELLO")
+assert(hello(str: nil) == "")
+
+// Optional Binding (guard)
+func hello2(str: String?) -> String {
+  let optionalstr: String? = str
+  guard let unwrappedstr = optionalstr else { return "" }
+  return unwrappedstr.uppercased()
+}
+assert(hello2(str: "hello") == "HELLO")
+assert(hello2(str: nil) == "")
+
+// Optional Chaining
+var optionalStr: String?
+assert(optionalStr?.uppercased() == nil)
+
+optionalStr = "hello"
+assert(optionalStr?.uppercased() == "HELLO")
+
+// Forced Unwrapping
+var optionalStr2: String? = "hello"
+assert(optionalStr2!.uppercased() == "HELLO")
+// var optionalStr3: String? = nil
+// optionalStr3!.uppercased()
+
+// ImplicitlyUnwrappedOptional型
+var implicitlyStr: String!
+implicitlyStr = "hello"
+assert(implicitlyStr.uppercased() == "HELLO")
+
+// Tuple
+let steve = ("Steve Jobs", 56)
+assert(steve.0 == "Steve Jobs")
+
+let (name, agee) = steve
+assert(name == "Steve Jobs")
+assert(agee == 56)
+
+let steve2 = (name: "Steve Jobs", age: 56)
+assert(steve2.age == 56)
+
+// Set
+let fishes: Set<String> = ["Mackerel", "Saury", "Sardine"]
+assert(fishes.contains("Mackerel") == true)
+
+// 三項演算子
+let someValue = true ? 1 : 0
+assert(someValue == 1)
+
+// nil 結合演算子
+var optionalInt2: Int?
+let someValue2 = optionalInt2 ?? 0
+assert(someValue2 == 0)
+
+// `let a?` のようにして nil チェックできる
+func hoge(pair: (String?, String?)) -> String {
+  switch pair {
+  case let (a?, b?):
+    return "\(a) and \(b)"
+  case let (a?, _):
+    return a
+  case let (_, b?):
+    return b
+  case (_, _):
+    return "No man"
+  }
+}
+assert(hoge(pair: ("Steve", "Bill")) == "Steve and Bill")
+assert(hoge(pair: ("Steve", nil)) == "Steve")
+assert(hoge(pair: (nil, "Bill")) == "Bill")
+assert(hoge(pair: (nil, nil)) == "No man")
+
+// part 2
+do {
+  var result: [Int] = []
+  for case let i? in [0, nil, 3, 4, nil] {
+    result += [i]
+  }
+  assert(result == [0, 3, 4])
+}
+
+// part 3
+do {
+  // 実行しないと、Optional かどうかわからないから ? がないとエラー（多分）
+  var result: [Optional<Int>] = []
+  for i in [0, nil, 3, 4, nil] where i != nil {
+    result += [i]
+  }
+  assert(result == [0, 3, 4])
+}
+
+// 可変長引数
+do {
+  // `_` つけるとkey省略して渡せる
+  func sum(_ numbers: Int...) -> Int {
+      return numbers.reduce(0, +)
+  }
+  assert(sum(1, 4, 7) == 12)
+}
+
+// 関数型
+do {
+  func multiply(number first: Int, by second: Int) -> Int {
+    return first * second
+  }
+  var calculation: (Int, Int) -> Int
+  calculation = multiply
+  assert(calculation(3, 7) == 21)
+}
+do {
+  func calculation(kind: String) -> (Int, Int) -> Int {
+    func multiply(number: Int, by: Int) -> Int { return number * by }
+    func add(number: Int, to: Int) -> Int { return number + to }
+
+    switch kind {
+    case "add":
+      return add
+    case "multiply":
+      return multiply
+    default:
+      fatalError("Unsupported")
+    }
+  }
+  assert(calculation(kind: "add")(2, 4) == 6)
+  assert(calculation(kind: "multiply")(2, 4) == 8)
+}
+
+// closure
+do {
+  func doubling(number: Int) -> Int {
+    return 2 * number
+  }
+  assert([0, 1, 2, 3, 4, 5].map(doubling) == [0, 2, 4, 6, 8, 10])
+}
+do {
+  assert(
+    [0, 1, 2, 3, 4, 5].map({ (number: Int) -> Int in
+      return 2 * number
+    })
+    == [0, 2, 4, 6, 8, 10]
+  )
+}
+do {
+  assert([0, 1, 2, 3, 4, 5].map({ number in 2 * number }) == [0, 2, 4, 6, 8, 10])
+}
+do {
+  assert([0, 1, 2, 3, 4, 5].map({ 2 * $0 }) == [0, 2, 4, 6, 8, 10])
+}
+do {
+  assert([0, 1, 2, 3, 4, 5].map{ 2 * $0 } == [0, 2, 4, 6, 8, 10])
+}
+
+// enum
+enum ArithmeticOperation {
+    case add
+    case subtract
+    case multiply
+    case divide
+}
+enum Diagram {
+    case line(Double)
+    case rectangle(Double, Double)
+    case circle(Double)
+}
+func calculateArea(of diagram: Diagram) -> Double {
+    let area: Double
+    switch diagram {
+    case .line(_):
+        area = 0.0
+    case let .rectangle(width, height):
+        area = width * height
+    case .circle(let radius):
+        area = radius * radius * Double.pi
+    }
+    return area
+}
+assert(calculateArea(of: .line(3.0)) == 0.0)
+assert(calculateArea(of: .rectangle(2.0, 3.0)) == 6.0)
+assert(calculateArea(of: .circle(3.0)) == 3.0 * 3.0 * Double.pi)
+
+// struct
+do {
+  struct Body {
+    let height: Double
+    let mass: Double
+  }
+  let myBody = Body(height: 129.3, mass: 129.3)
+  func calculateBodyMassIndex(of body: Body) -> Double {
+    let meterHeight = body.height / 100.0
+    return body.mass / (meterHeight * meterHeight)
+  }
+  assert(calculateBodyMassIndex(of: myBody) == 77.33952049497292)
+}
+
+// Class
+do {
+  class Lot {
+    var remains: [String]
+    init(_ elements: String...) {
+      self.remains = elements
+    }
+    func choose() -> String? {
+      if remains.isEmpty {
+        return nil
+      }
+      let randomIndex = Int.random(in: 0 ..< remains.count)
+      return remains.remove(at: randomIndex)
+    }
+  }
+  func pick(from lot: Lot, count: Int) -> [String] {
+    var result: [String] = []
+    for _ in (0..<count) {
+      lot.choose().map { result.append($0) }
+    }
+    return result
+  }
+  let lot = Lot("Swift", "Objective-C", "Java", "Scala", "Perl", "Ruby")
+  print(pick(from: lot, count: 3))
+  print(lot.remains)
+}
